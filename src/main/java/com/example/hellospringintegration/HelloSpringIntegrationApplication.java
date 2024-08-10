f8075f20d3474896423e67d8a9332b26ce896a5d
qsunki <ajsthfldu@outlook.com>
2024-08-11 00:47:06 +0900

    initial commit

---
+++ src/main/java/com/example/hellospringintegration/HelloSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellospringintegration/HelloSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
