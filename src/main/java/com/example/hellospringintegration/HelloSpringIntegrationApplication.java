2ed75d0a0f77592f70b2b48bb7e7b924190b2504
qsunki <ajsthfldu@outlook.com>
2024-10-30 23:32:56 +0900

    configuration

--- src/main/java/com/example/hellospringintegration/HelloSpringIntegrationApplication.java
+++ src/main/java/com/example/hellospringintegration/HelloSpringIntegrationApplication.java
@@ -1,13 +1,18 @@
 package com.example.hellospringintegration;
 
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
+@IntegrationComponentScan
 public class HelloSpringIntegrationApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(HelloSpringIntegrationApplication.class, args);
+        // for remaining running state
+        //noinspection ResultOfMethodCallIgnored
+        System.in.read();
     }
-
 }
