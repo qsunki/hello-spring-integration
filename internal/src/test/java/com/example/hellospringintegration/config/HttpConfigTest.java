47f933552d2581225e9dd49e0d4a6bf58e23e190
qsunki <ajsthfldu@outlook.com>
2024-12-01 15:22:14 +0900

    Disable Spring Integration Http test

--- internal/src/test/java/com/example/hellospringintegration/config/HttpConfigTest.java
+++ internal/src/test/java/com/example/hellospringintegration/config/HttpConfigTest.java
@@ -7,8 +7,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@Disabled
 class HttpConfigTest {
 
     @Test
