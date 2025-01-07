4209fef45a4dcc1ee7f2229594de70154dd16e6f
qsunki <ajsthfldu@outlook.com>
2025-01-08 02:18:53 +0900

    Add PollerApp and PollerConfig for message polling setup

---
+++ helloworld/src/main/java/com/example/PollerApp.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class PollerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PollerConfig.class);
+    }
+}
---
+++ helloworld/src/main/java/com/example/PollerConfig.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableIntegration
+public class PollerConfig {
+
+    @Bean
+    public MessageChannel loggerChannel() {
+        return new DirectChannel();
+    }
+
+    @InboundChannelAdapter(
+            channel = "loggerChannel",
+            poller = @Poller(fixedDelay = "2000", maxMessagesPerPoll = "2"))
+    public long timeMessageSource() {
+        return System.currentTimeMillis();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(20);
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "loggerChannel")
+    public LoggingHandler loggingHandler() {
+        LoggingHandler adapter = new LoggingHandler(LoggingHandler.Level.INFO);
+        adapter.setLoggerName("org.springframework.integration.samples.helloworld");
+        adapter.setLogExpressionString("headers.id + ': ' + payload");
+        return adapter;
+    }
+}
